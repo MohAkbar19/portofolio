60409c2cd36a7347b7902287ca6ec8892537e06b
Nur Arini <nurarini0302@gmail.com>
2024-09-26 15:56:59 +0700

    [ADD] add spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
-      <h1 className="text-white">Hello World</h1>
-      <h1 className="text-white">Hello World</h1>
+      <Hero />
     </main>
   );
 }
