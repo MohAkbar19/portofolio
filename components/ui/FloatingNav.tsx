f50d839cd53af4b7a605da940f89a1c4a5dd15d2
Nur Arini <nurarini0302@gmail.com>
2024-11-01 22:48:06 +0700

    [UP] update font

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -72,7 +72,7 @@ export const FloatingNav = ({
               "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-            <span className=" text-sm !cursor-pointer">{navItem.name}</span>
+            <span className=" text-sm !cursor-pointer font-montserrat tracking-wider">{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
